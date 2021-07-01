8c8ffa265d8c8e03d49957f5c47d7e2abdc814ad
Bishtawi <bishtawi@users.noreply.github.com>
2021-06-30 20:45:16 -0700

    Day 6

---
+++ src/day06.rs
@@ -0,0 +1,64 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const INPUT: &str = "input/day06.txt";
+
+pub fn solve() {
+    println!("{}: {} | {}", INPUT, part1(), part2());
+}
+
+fn part1() -> u64 {
+    let mut total: u64 = 0;
+    let mut group: HashSet<char> = HashSet::new();
+
+    let file = File::open(INPUT).unwrap();
+    let reader = BufReader::new(file);
+    for (_i, line) in reader.lines().enumerate() {
+        let line: Vec<char> = line.unwrap().chars().collect();
+
+        if line.len() == 0 {
+            total += group.len() as u64;
+            group = HashSet::new();
+            continue;
+        }
+
+        for character in line {
+            group.insert(character);
+        }
+    }
+    total += group.len() as u64;
+    return total;
+}
+
+fn part2() -> u64 {
+    let mut total: u64 = 0;
+    let mut group: HashSet<char> = HashSet::new();
+    let mut flag = true;
+
+    let file = File::open(INPUT).unwrap();
+    let reader = BufReader::new(file);
+    for (_i, line) in reader.lines().enumerate() {
+        let line: Vec<char> = line.unwrap().chars().collect();
+
+        if line.len() == 0 {
+            total += group.len() as u64;
+            flag = true;
+            continue;
+        }
+
+        let mut person: HashSet<char> = HashSet::new();
+        for character in line {
+            person.insert(character);
+        }
+
+        if flag {
+            flag = false;
+            group = person;
+        } else {
+            group = group.intersection(&person).map(|&x| x.clone()).collect();
+        }
+    }
+    total += group.len() as u64;
+    return total;
+}
